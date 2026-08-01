f041a2624085338a3bceacbbdd516d194055c0bc
ProDreVil <leejamesbenigno59@gmail.com>
2026-08-01 10:45:34 +0800

    Updated

--- src/main.py
+++ src/main.py
@@ -8,7 +8,10 @@ audio, sample_rate = load_audio("assets/sample/sample.wav")
 frequencies = detect_pitch(audio)
 notes = extract_notes(frequencies, sample_rate)
 
-for i, freq in enumerate(frequencies[:20]):
-    if not np.isnan(freq):
-        time = i * config.HOP_LENGTH / sample_rate
-        print(f"{time:.2f}s - {freq:.2f} Hz - {i} - {notes[i].name}")
+# for i, freq in enumerate(frequencies[:20]):
+#     if not np.isnan(freq):
+#         time = i * config.HOP_LENGTH / sample_rate
+#         print(f"{time:.2f}s - {freq:.2f} Hz - {i} - {notes[i].name}")
+
+for note in notes[:20]:
+    print(note)
