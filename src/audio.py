59431f0ea42ade3b99693f1d051eff1dfd69f2db
ProDreVil <leejamesbenigno59@gmail.com>
2026-08-01 15:23:57 +0800

    Set up mic

--- src/audio.py
+++ src/audio.py
@@ -17,19 +17,12 @@ def load_audio(file_path):
 
 def plot_waveform(audio, sample_rate):
     plt.figure(figsize=(12, 4))
-
-    librosa.display.waveshow(
-        audio,
-        sr=sample_rate
-    )
-
+    librosa.display.waveshow(audio, sr=sample_rate)
     plt.title("Audio Waveform")
     plt.xlabel("Time (seconds)")
     plt.ylabel("Amplitude")
-
     plt.tight_layout()
     plt.show()
-
 
 if __name__ == "__main__":
     audio, sample_rate = load_audio("assets/sample/sample.wav")
