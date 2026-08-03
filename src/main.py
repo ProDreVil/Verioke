d65ce1e5c8622f137bb0c6a9eaf1e3809840fe25
ProDreVil <leejamesbenigno59@gmail.com>
2026-08-03 13:08:27 +0800

    Debugging

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from reference import load_reference
 from recorder import record_audio, list_input_devices
-from scorer import compare_notes
+from scorer import compare_notes, calculate_pitch_accuracy
 from utils import process_audio
 
 reference_notes = load_reference("assets/songs/beer")
@@ -8,10 +8,21 @@ reference_notes = load_reference("assets/songs/beer")
 singer_notes = process_audio("recordings/take1.wav")
 
 matches = compare_notes(reference_notes, singer_notes)
+score = calculate_pitch_accuracy(matches)
 
-for reference, singer in matches[:10]:
-    print(f"{reference.pitch:>3} ({reference.time:.2f}s)" f" -> "f"{singer.pitch:>3} ({singer.time:.2f}s)")
+print(f"Reference Notes: {len(reference_notes)}")
+print(f"Singer Notes: {len(singer_notes)}")
+print(f"Matched Notes: {len(matches)}")
 
+# for reference, singer in matches[:10]:
+#     print(f"{reference.pitch:>3} ({reference.time:.2f}s)" f" -> "f"{singer.pitch:>3} ({singer.time:.2f}s)")
+
+for reference, singer in matches:
+    print(
+        f"Ref: {reference.pitch:>3} ({reference.midi}) "
+        f"-> "
+        f"Singer: {singer.pitch:>3} ({singer.midi})"
+    )
 
 # list_input_devices()
 
--- src/scorer.py
+++ src/scorer.py
@@ -5,10 +5,7 @@ def compare_notes(reference_notes: list[Note], singer_notes: list[Note]) -> list
     matches = []
     reference_index = 0
     singer_index = 0
-    while (
-        reference_index < len(reference_notes)
-        and singer_index < len(singer_notes)
-    ):
+    while (reference_index < len(reference_notes) and singer_index < len(singer_notes)):
         reference = reference_notes[reference_index]
         singer = singer_notes[singer_index]
         difference = abs(reference.time - singer.time)
@@ -21,3 +18,13 @@ def compare_notes(reference_notes: list[Note], singer_notes: list[Note]) -> list
         else:
             reference_index += 1
     return matches
+
+def calculate_pitch_accuracy(matches: list[tuple[Note, Note]]) -> float:
+    if not matches:
+        return 0.0
+    correct = 0
+    for reference, singer in matches:
+        if reference.midi == singer.midi:
+            correct += 1
+    print(f"Correct Notes: {correct}")
+    return (correct/len(matches)) * 100
